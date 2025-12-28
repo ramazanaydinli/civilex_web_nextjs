24b033d5619e22006cae5db9281d6e7f52fbca30
Ramazan <ramazanaydinli94@gmail.com>
2025-12-28 05:50:02 +0300

    github settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Statik HTML çıktısı için
+  images: {
+    unoptimized: true, // GitHub Pages'da resim optimizasyonu için
+  },
+  // GitHub Repo adınız buraya ekleniyor:
+  basePath: '/civilex_web_nextjs',
+  
+  // Asset'lerin (CSS, JS) doğru yoldan yüklenmesi için:
+  assetPrefix: '/civilex_web_nextjs',
 };
 
 export default nextConfig;
